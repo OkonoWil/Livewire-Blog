3e14776300840bb426b416deb5781edf692d4a18
OkonoWil <okonowilfried@gmail.com>
2024-04-30 05:19:16 +0100

    fix config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+import '../dist/css/demo.min.css';
+import '../dist/css/tabler.min.css';
+import '../dist/css/tabler-vendors.min.css';
+
+import '../dist/js/demo.min.js;';
+import '../dist/js/demo-theme.min.js';
+import '../dist/js/tabler.min.js';
+
+
+import.meta.glob([
+    '../static/**',
+]);
